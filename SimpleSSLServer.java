cc55c7052dde743d10dcd395def466a3978afb28
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2024-04-03 14:12:59 -0600

    Update SimpleSSLServer.java

--- SimpleSSLServer.java
+++ SimpleSSLServer.java
@@ -9,136 +9,104 @@ import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.HexFormat;
 
-public class MySSLServer {
+public class SimpleSSLServer {
     public static void main(String[] args) throws Exception {
+        // Load server certificate and private key for SSL communication
         X509Certificate serverCert = SSLUtils.loadCertificate("serverCert.pem");
         PrivateKey serverPrivateKey = SSLUtils.readPKCS8PrivateKey("serverKey.pem");
 
+        // Setup server socket on specified port
         int port = 12343;
-        X509Certificate clientCert = null;
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("Server listening on port " + port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server listening on port " + port);
 
-        try (Socket clientSocket = serverSocket.accept()) {
-            System.out.println("Client connected.");
+            // Accept connection from client
+            try (Socket clientSocket = serverSocket.accept()) {
+                System.out.println("Client connected.");
 
-            DataOutputStream dOut = new DataOutputStream(clientSocket.getOutputStream());
-            DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
+                // Setup input and output streams for data exchange
+                DataOutputStream dOut = new DataOutputStream(clientSocket.getOutputStream());
+                DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
 
-            // Send server's certificate
-            byte[] certBytes = Files.readAllBytes(Paths.get("serverCert.pem"));
-            dOut.writeInt(certBytes.length);
-            dOut.write(certBytes);
-            dOut.flush();
-            System.out.println("Server's certificate sent to client.");
-
-            // Receive client's certificate
-            int length = dIn.readInt();
-            if (length > 0) {
-                byte[] clientCertBytes = new byte[length];
-                dIn.readFully(clientCertBytes);
-                CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-                InputStream certStream = new ByteArrayInputStream(clientCertBytes);
-                clientCert = (X509Certificate) certFactory.generateCertificate(certStream);
-                System.out.println("Received client's certificate.");
-                // Validating Client Certificate
-            }
-
-            // Receive encryption and integrity choices from client
-            length = dIn.readInt();
-            String algoMessage = null;
-            if (length > 0) {
-                byte[] algoBytes = new byte[length];
-                dIn.readFully(algoBytes);
-                algoMessage = new String(algoBytes);
-                System.out.println("Received algorithm choices: " + algoMessage);
-                // Acknowledge Client Algorithm choices
-            }
-
-            // After receiving the algoMessage from the client
-            length = dIn.readInt();
-            byte[] encryptedClientNonce = null;
-            byte[] clientNonce = null;
-            byte[] encryptedServerNonce = null;
-            byte[] masterSecret = null;
-            long serverNonce = 0;
-            if (length > 0) {
-                encryptedClientNonce = new byte[length];
-                dIn.readFully(encryptedClientNonce);
-                clientNonce = SSLUtils.decryptData(encryptedClientNonce, serverPrivateKey);
-                System.out.println("Received and decrypted client's nonce.");
-
-                // Generate server's nonce and send it encrypted to the client
-                serverNonce = System.currentTimeMillis();
-                assert clientCert != null;
-                encryptedServerNonce = SSLUtils.encryptData(Long.toString(serverNonce).getBytes(), clientCert.getPublicKey());
-                dOut.writeInt(encryptedServerNonce.length);
-                dOut.write(encryptedServerNonce);
+                // Send server's certificate to client
+                byte[] certBytes = Files.readAllBytes(Paths.get("serverCert.pem"));
+                dOut.writeInt(certBytes.length);
+                dOut.write(certBytes);
                 dOut.flush();
-                System.out.println("Encrypted server nonce sent to client.");
+                System.out.println("Server's certificate sent to client.");
 
-                // XOR nonce to create master secret
-                masterSecret = SSLUtils.xorBytes(Long.toString(serverNonce).getBytes(), clientNonce);
-                // Use masterSecret for further communication
-            }
-
-            // After sending or receiving each piece of data, appending it to the `messages`
-            String messages = String.valueOf(serverCert) +
-                    clientCert +
-                    algoMessage +
-                    Arrays.toString(clientNonce) +
-                    Arrays.toString(encryptedClientNonce) +
-                    serverNonce +
-                    Arrays.toString(encryptedServerNonce);
-
-            byte[] messagesBytes = messages.getBytes();
-
-
-            // Receive and verify client's hash
-            int len = dIn.readInt();
-            if (len > 0) {
-                byte[] clientHash = new byte[len];
-                dIn.readFully(clientHash);
-
-                System.out.println("Client's hash received and verified.");
-            }
-
-            // Compute and send hash of all messages appended with "SERVER"
-            byte[] serverHash = SSLUtils.computeKeyedHash(messagesBytes, "SERVER");
-            dOut.writeInt(serverHash.length);
-            dOut.write(serverHash);
-            dOut.flush();
-
-            byte[] encryptionKeyServerToClient = SSLUtils.KeyGenerator.deriveKey(masterSecret, "encryptionS2C");
-            byte[] authenticationKeyServerToClient = SSLUtils.KeyGenerator.deriveKey(masterSecret, "authenticationS2C");
-
-            File inputFile = new File("/Users/vt003/IdeaProjects/PA2/src/InitialMessage.txt");
-            File encryptedFile = new File("/Users/vt003/IdeaProjects/PA2/src/EncryptedInitialMessage");
-
-            SSLUtils.CryptoUtils.EncryptionUtil.encryptFile(encryptionKeyServerToClient, inputFile.getAbsolutePath(), encryptedFile.getAbsolutePath());
-
-            byte[] fileContent = Files.readAllBytes(inputFile.toPath());
-            byte[] fileHash = SSLUtils.computeKeyedHash(fileContent, HexFormat.of().formatHex(authenticationKeyServerToClient));
-
-            dOut.writeInt(fileHash.length);
-            dOut.write(fileHash);
-
-            try (FileInputStream fileIn = new FileInputStream(encryptedFile);
-                 BufferedInputStream bufferedIn = new BufferedInputStream(fileIn);
-                 DataOutputStream dataOut = new DataOutputStream(clientSocket.getOutputStream())) {
-
-                byte[] buffer = new byte[4096];
-                int bytesRead;
-                while ((bytesRead = bufferedIn.read(buffer)) != -1) {
-                    dataOut.writeInt(bytesRead);
-                    dataOut.write(buffer, 0, bytesRead);
+                // Receive and load client's certificate
+                int length = dIn.readInt();
+                if (length > 0) {
+                    byte[] clientCertBytes = new byte[length];
+                    dIn.readFully(clientCertBytes);
+                    X509Certificate clientCert = (X509Certificate) CertificateFactory.getInstance("X.509")
+                                                    .generateCertificate(new ByteArrayInputStream(clientCertBytes));
+                    System.out.println("Received client's certificate.");
+                    // Client certificate can now be validated or used for encryption
                 }
-                dataOut.writeInt(-1); // Signal end of file transmission
-            }
-            System.out.println("Encrypted file sent to client.");
 
-        } finally {
-            serverSocket.close();
+                // Receive client's encryption and integrity preferences
+                length = dIn.readInt();
+                String algoMessage = "";
+                if (length > 0) {
+                    byte[] algoBytes = new byte[length];
+                    dIn.readFully(algoBytes);
+                    algoMessage = new String(algoBytes);
+                    System.out.println("Received algorithm choices: " + algoMessage);
+                }
+
+                // Process client's nonce, generate server's nonce, create master secret
+                length = dIn.readInt();
+                if (length > 0) {
+                    byte[] encryptedClientNonce = new byte[length];
+                    dIn.readFully(encryptedClientNonce);
+                    byte[] clientNonce = SSLUtils.decryptData(encryptedClientNonce, serverPrivateKey);
+                    System.out.println("Received and decrypted client's nonce.");
+
+                    long serverNonce = System.currentTimeMillis();
+                    byte[] encryptedServerNonce = SSLUtils.encryptData(Long.toString(serverNonce).getBytes(), SSLUtils.loadCertificate("clientCert.pem").getPublicKey());
+                    dOut.writeInt(encryptedServerNonce.length);
+                    dOut.write(encryptedServerNonce);
+                    dOut.flush();
+                    System.out.println("Encrypted server nonce sent to client.");
+
+                    byte[] masterSecret = SSLUtils.xorBytes(Long.toString(serverNonce).getBytes(), clientNonce);
+                }
+
+                // Compute and send hash of all messages for integrity check
+                // Note: Actual verification should be implemented here based on the real application requirements
+                String messages = Arrays.toString(certBytes) + algoMessage; // Simplified example
+                byte[] serverHash = SSLUtils.computeKeyedHash(messages.getBytes(), "SERVER");
+                dOut.writeInt(serverHash.length);
+                dOut.write(serverHash);
+                dOut.flush();
+                System.out.println("Server's hash sent to client.");
+
+                // Encrypt and send a file to client
+                // Note: For simplicity, the file path is hardcoded. Consider making it configurable.
+                String filePath = "InitialMessage.txt"; // Example file path
+                File inputFile = new File(filePath);
+                byte[] encryptionKey = SSLUtils.KeyGenerator.deriveKey(masterSecret, "encryptionS2C");
+                byte[] fileContent = Files.readAllBytes(inputFile.toPath());
+                byte[] fileHash = SSLUtils.computeKeyedHash(fileContent, HexFormat.of().formatHex(encryptionKey));
+                dOut.writeInt(fileHash.length);
+                dOut.write(fileHash);
+
+                // Encrypt and send the file content
+                File encryptedFile = new File("Encrypted" + inputFile.getName());
+                SSLUtils.CryptoUtils.EncryptionUtil.encryptFile(encryptionKey, inputFile.getAbsolutePath(), encryptedFile.getAbsolutePath());
+                try (FileInputStream fileIn = new FileInputStream(encryptedFile)) {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = fileIn.read(buffer)) != -1) {
+                        dOut.writeInt(bytesRead);
+                        dOut.write(buffer, 0, bytesRead);
+                    }
+                }
+                dOut.writeInt(-1); // Signal end of file transmission
+                System.out.println("Encrypted file sent to client.");
+            }
         }
     }
 }
